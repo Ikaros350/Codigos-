8d348665350f489611e4020f763db1f4a07ea2a6
Santiago350 <47002827+Santiago350@users.noreply.github.com>
2019-01-24 22:21:47 -0500

    Add files via upload

---
+++ Desafio T2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Ingresar t, a:");
+
+            double t = double.Parse(Console.ReadLine()); // t = 3.61
+            double a = double.Parse(Console.ReadLine()); // a = 33.69
+
+            double y = t * Math.Sin(a * (Math.PI / 180));
+            double z = t * Math.Cos(a * (Math.PI / 180));
+            double b = Math.Asin((t * Math.Sin(a * (Math.PI / 180))) / y) * (180 / Math.PI); // ley de seno
+            double c = Math.Asin((z * Math.Sin(b * (Math.PI / 180))) / t) * (180 / Math.PI); //ley de seno
+
+            //aca tuve que convertir a b en radianes "Math.Sin(b*(Math.PI/180))"
+
+            Console.WriteLine("y=" + y);
+            Console.WriteLine("z=" + z);
+            Console.WriteLine("b=" + b);
+            Console.WriteLine("c=" + c);
+        }
+    }
+}
---
+++ Desafio T3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Ingresar c, z:");
+
+            double c = double.Parse(Console.ReadLine()); // c = 56.31
+            double z = double.Parse(Console.ReadLine()); // z = 3
+
+            double t = z / Math.Sin(c * (Math.PI / 180));
+            double y = t * Math.Cos(c * (Math.PI / 180));
+            double b = Math.Asin((t * Math.Sin(c * (Math.PI / 180))) / z) * (180 / Math.PI); // ley de seno y tranformacion a grados
+            double a = Math.Asin((y * Math.Sin(b * (Math.PI / 180))) / t) * (180 / Math.PI); // ley de seno y tranformacion a grados
+
+            Console.WriteLine("t=" + t);
+            Console.WriteLine("y=" + y);
+            Console.WriteLine("b=" + b);
+            Console.WriteLine("a=" + a);
+        }
+    }
+}
